d6b71889a8f0e3a21ee60ba6453c5d97c24d0e42
Thomas <thomasweems3@gmail.com>
2019-01-25 11:43:58 -0600

    trying to fix nav bug

--- components/LandingNav.vue
+++ components/LandingNav.vue
@@ -3,33 +3,32 @@
     toggleable="md" 
     type="light" 
     variant="white">
-    <div class="container">
-      <b-navbar-brand href="#">
-        <img 
-          class="hero-logo"
-          src="~assets/TraxitHeroLogo.png"
-        >
-      </b-navbar-brand>
-      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-      <b-collapse 
-        id="nav_collapse"
-        is-nav 
+    <b-navbar-brand href="#">
+      <img 
+        class="hero-logo"
+        src="~assets/TraxitHeroLogo.png"
       >
-        <b-navbar-nav class="ml-auto">
-          <b-nav-item href="#mission">Our Mission</b-nav-item>
-          <b-nav-item to="/features/overview">Features</b-nav-item>
-          <b-nav-item href="#pricing">Pricing</b-nav-item>
-          <b-nav-item href="#contact">Contact</b-nav-item>
-          <b-nav-item to="/support">Support</b-nav-item>
-          <b-nav-item 
-            class="login-btn"
-            to="/login"
-          >
-            Login
-          </b-nav-item>
-        </b-navbar-nav>
-      </b-collapse>
-    </div>
+    </b-navbar-brand>
+    <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+    <b-collapse 
+      id="nav_collapse"
+      is-nav
+      style 
+    >
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item href="#mission">Our Mission</b-nav-item>
+        <b-nav-item to="/features/overview">Features</b-nav-item>
+        <b-nav-item href="#pricing">Pricing</b-nav-item>
+        <b-nav-item href="#contact">Contact</b-nav-item>
+        <b-nav-item to="/support">Support</b-nav-item>
+        <b-nav-item 
+          class="login-btn"
+          to="/login"
+        >
+          Login
+        </b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
   </b-navbar>
 </template>
 
@@ -40,10 +39,6 @@ export default {
 </script>
 
 <style>
-.navbar-container {
-  width: 100%;
-}
-
 nav {
   padding: 10px 50px !important;
 }
