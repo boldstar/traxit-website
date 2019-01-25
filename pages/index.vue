ff0bfe77a369050fbc091e2e6ef4b65d8d707bad
Thomas <thomasweems3@gmail.com>
2019-01-25 10:03:28 -0600

    trying to fix nav bug

--- pages/index.vue
+++ pages/index.vue
@@ -2,36 +2,38 @@
   <div 
     class="landing-page"
   >
-    <b-navbar 
-      toggleable="md" 
-      type="light" 
-      variant="white">
-      <b-navbar-brand href="#">
-        <img 
-          class="hero-logo"
-          src="~assets/TraxitHeroLogo.png"
-        >
-      </b-navbar-brand>
-      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-      <b-collapse 
-        id="nav_collapse"
-        is-nav 
-      >
-        <b-navbar-nav class="ml-auto">
-          <b-nav-item href="#mission">Our Mission</b-nav-item>
-          <b-nav-item to="/features/overview">Features</b-nav-item>
-          <b-nav-item href="#pricing">Pricing</b-nav-item>
-          <b-nav-item href="#contact">Contact</b-nav-item>
-          <b-nav-item to="/support">Support</b-nav-item>
-          <b-nav-item 
-            class="login-btn"
-            to="/login"
+    <no-ssr>
+      <b-navbar 
+        toggleable="md" 
+        type="light" 
+        variant="white">
+        <b-navbar-brand href="#">
+          <img 
+            class="hero-logo"
+            src="~assets/TraxitHeroLogo.png"
           >
-            Login
-          </b-nav-item>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
+        </b-navbar-brand>
+        <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+        <b-collapse 
+          id="nav_collapse"
+          is-nav 
+        >
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item href="#mission">Our Mission</b-nav-item>
+            <b-nav-item to="/features/overview">Features</b-nav-item>
+            <b-nav-item href="#pricing">Pricing</b-nav-item>
+            <b-nav-item href="#contact">Contact</b-nav-item>
+            <b-nav-item to="/support">Support</b-nav-item>
+            <b-nav-item 
+              class="login-btn"
+              to="/login"
+            >
+              Login
+            </b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </no-ssr>
     <section 
       id="hero"
       class="hero" 
