fcce22cc3d4983c6b21188e71299612266d4f187
Thomas <thomasweems3@gmail.com>
2019-01-25 10:31:48 -0600

    trying to fix nav bug

---
+++ components/LandingNav.vue
@@ -0,0 +1,75 @@
+<template>
+  <b-navbar 
+    toggleable="md" 
+    type="light" 
+    variant="white">
+    <b-navbar-brand href="#">
+      <img 
+        class="hero-logo"
+        src="~assets/TraxitHeroLogo.png"
+      >
+    </b-navbar-brand>
+    <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+    <b-collapse 
+      id="nav_collapse"
+      is-nav 
+    >
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item href="#mission">Our Mission</b-nav-item>
+        <b-nav-item to="/features/overview">Features</b-nav-item>
+        <b-nav-item href="#pricing">Pricing</b-nav-item>
+        <b-nav-item href="#contact">Contact</b-nav-item>
+        <b-nav-item to="/support">Support</b-nav-item>
+        <b-nav-item 
+          class="login-btn"
+          to="/login"
+        >
+          Login
+        </b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+    name: 'LandingNav'
+}
+</script>
+
+<style>
+
+
+nav {
+  padding: 10px 50px !important;
+}
+
+li a {
+  text-decoration: none;
+  margin-left: 25px;
+  letter-spacing: 2px;
+  color: var(--gray-color);
+  font-weight: 600;
+}
+
+li a:hover {
+  color: var(--blue-color);
+  text-decoration: none;
+}
+
+.login-btn a {
+  color: var(--pink-color) !important;
+  border: 3px solid var(--blue-color)!important;
+  border-radius: 10px !important;
+  padding: 8px 25px !important;
+  font-weight: 600 !important;
+}
+
+.login-btn a:hover {
+  background-color: var(--blue-color);
+  color: white !important;
+}
+ 
+</style>
+
+
--- pages/index.vue
+++ pages/index.vue
@@ -3,36 +3,7 @@
     class="landing-page"
   >
     <header>
-      <b-navbar 
-        toggleable="md" 
-        type="light" 
-        variant="white">
-        <b-navbar-brand href="#">
-          <img 
-            class="hero-logo"
-            src="~assets/TraxitHeroLogo.png"
-          >
-        </b-navbar-brand>
-        <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-        <b-collapse 
-          id="nav_collapse"
-          is-nav 
-        >
-          <b-navbar-nav class="ml-auto">
-            <b-nav-item href="#mission">Our Mission</b-nav-item>
-            <b-nav-item to="/features/overview">Features</b-nav-item>
-            <b-nav-item href="#pricing">Pricing</b-nav-item>
-            <b-nav-item href="#contact">Contact</b-nav-item>
-            <b-nav-item to="/support">Support</b-nav-item>
-            <b-nav-item 
-              class="login-btn"
-              to="/login"
-            >
-              Login
-            </b-nav-item>
-          </b-navbar-nav>
-        </b-collapse>
-      </b-navbar>
+      <LandingNav />
     </header>
     <section 
       id="hero"
@@ -238,11 +209,12 @@
 </template>
 
 <script>
+import LandingNav from '~/components/LandingNav'
 
 export default {
   name: 'Index',
   components: {
- 
+    LandingNav
   },
   data() {
     return {
@@ -289,36 +261,6 @@ header {
   position: absolute;
   z-index: 1;
   width: 100%;
-}
-
-nav {
-  padding: 10px 50px !important;
-}
-
-li a {
-  text-decoration: none;
-  margin-left: 25px;
-  letter-spacing: 2px;
-  color: var(--gray-color);
-  font-weight: 600;
-}
-
-li a:hover {
-  color: var(--blue-color);
-  text-decoration: none;
-}
-
-.login-btn a {
-  color: var(--pink-color) !important;
-  border: 3px solid var(--blue-color)!important;
-  border-radius: 10px !important;
-  padding: 8px 25px !important;
-  font-weight: 600 !important;
-}
-
-.login-btn a:hover {
-  background-color: var(--blue-color);
-  color: white !important;
 }
 
 .hero-logo {
