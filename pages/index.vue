d23f059591abcccac8e89a7b32603c2014c65e3a
Thomas <thomasweems3@gmail.com>
2019-01-25 09:53:21 -0600

    trying to fix nav bug

--- pages/index.vue
+++ pages/index.vue
@@ -6,22 +6,17 @@
       toggleable="md" 
       type="light" 
       variant="white">
-
       <b-navbar-brand href="#">
         <img 
           class="hero-logo"
           src="~assets/TraxitHeroLogo.png"
         >
       </b-navbar-brand>
-
       <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-
       <b-collapse 
         id="nav_collapse"
         is-nav 
       >
-
-        <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
           <b-nav-item href="#mission">Our Mission</b-nav-item>
           <b-nav-item to="/features/overview">Features</b-nav-item>
@@ -35,7 +30,6 @@
             Login
           </b-nav-item>
         </b-navbar-nav>
-
       </b-collapse>
     </b-navbar>
     <section 
